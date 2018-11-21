673b3f2ca51271242fdc2ae23182076ebf7abf18
Kylan Robinson <kylanr@gmail.com>
2018-11-21 13:22:05 -0800

    Add some javascript to the home page

---
+++ js/attributes.js
@@ -0,0 +1,1 @@
+document.getElementById('attributes').innerHTML = "Radio amateur. WSU Cougar fan. Husband. Traveler. Runner. Soccer player. Uncle. CISSP. Son. Camper. Father. Trumpet player. American. Programmer. Christian. Rock, Paper, Scissors competitor. Foodie. Inventor. Engineer. Coach. Washington State University alumnus. Podcast producer. Brother.";
